7b709d9deabd805869111a933705ec5c154bcb1b
Piusshungu <shungupius@gmail.com>
2019-07-07 20:01:14 +0300

    added a login component

--- my_business-frontend/src/main.js
+++ my_business-frontend/src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
+
+Vue.use(VueRouter)
+
+import Login from './components/Login.vue';
+
+const routes = [
+  { path: '/', component: Login },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(Login)
 }).$mount('#app')
