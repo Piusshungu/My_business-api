dd2a4da715bba9b6950ae13d2b0c26c7902be4b8
Piusshungu <shungupius@gmail.com>
2019-07-24 17:00:20 +0300

    added a topbar on pages

---
+++ my_business-frontend/src/components/DailyProductions.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="toolbar">
+    <v-toolbar color="#009688" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn color="#009688">Logout
+          <v-icon>person</v-icon>
+      </v-btn>
+    </v-toolbar>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
