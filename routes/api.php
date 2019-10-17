34eb9cb71e9749151c3bff6da36bcb38e746f5db
System Administrator <root@Graysons-MacBook-Pro.local>
2019-10-17 17:23:45 +0300

    Added resources root for products Items

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/productsItems', 'ProductsController@AddProducts');
+Route::resource('/productsItems', 'ProductsItemsController');
