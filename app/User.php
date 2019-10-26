cc9e4d49eb0487dd64902dd7cf3fe658c03a39ef
System Administrator <root@Graysons-MacBook-Pro.local>
2019-10-26 11:11:06 +0300

    Added many to many relationship between users and roles

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Emadadly\LaravelUuid\Uuids;
+use App\Roles;
 
 class User extends Authenticatable
 {
@@ -39,4 +40,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function Roles(){
+        return $this
+        ->belongsToMany('App\Roles')
+        ->withTimestamps();
+
+    }
 }
