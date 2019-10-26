dbfb225373176ba81491dc14cf783de52e44c576
System Administrator <root@Graysons-MacBook-Pro.local>
2019-10-26 11:41:57 +0300

    Added functions to handle user permissions and previllages

--- app/User.php
+++ app/User.php
@@ -41,10 +41,39 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+// Creating many to many realationships with Roles
     public function Roles(){
         return $this
         ->belongsToMany('App\Roles')
         ->withTimestamps();
+
+    }
+    public function authorizeRoles($roles){
+        if($this->hasAnyrole($roles)){
+            return true;
+        }
+        abort(404, 'This action is Unauthorized');
+    }
+    public function hasAnyRole($roles){
+        if(is_array($roles)){
+            foreach($roles as $role){
+                if($this->hasRole($role)){
+                    return true;
+                }
+            }
+        }   
+        else{
+            if($this->hasRole($roles)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public function hasRole($role){
+        if($this->role()->where('name', $role)->first()){
+            return true;
+        }
+        return false;
 
     }
 }
