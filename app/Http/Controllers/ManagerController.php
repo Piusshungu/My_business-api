2746416d55a6a3e145d665a8d4027c2f222ad924
Piusshungum <shungupius@gmail.com>
2019-12-09 14:19:11 +0300

    Modified manager controller

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ManagerController extends Controller
 {
-    //
+    //Index method for Manager
+    public function Index(){
+        return new ProductsCollection(Products::all());
+    }
 }
